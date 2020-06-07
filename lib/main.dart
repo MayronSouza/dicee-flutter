8d8b93b3058870532906d70c3958e9ac942ea3cc
MayronGMZ <mayron.souza@gmz.solutions>
2020-06-07 15:47:56 -0300

    Print no console ao clicar nas imagens

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(
-    MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red,
-        appBar: AppBar(
-          title: Text('Dicee'),
+      MaterialApp(
+        home: Scaffold(
           backgroundColor: Colors.red,
+          appBar: AppBar(
+            title: Text('Dicee'),
+            backgroundColor: Colors.red,
+          ),
+          body: DicePage(),
         ),
-        body: DicePage(),
       ),
-    ),
-  );
+    );
 
 class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () => print('Left button on pressed!'),
+              child: Image.asset('assets/images/dice1.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () => print('Right button on pressed!'),
+              child: Image.asset('assets/images/dice1.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
